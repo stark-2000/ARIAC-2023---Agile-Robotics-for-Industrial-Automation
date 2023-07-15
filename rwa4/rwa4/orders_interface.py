84c81958918ba40011d29fcef6de481a69bc2ce6
Ian Serbin <iserbin01@gmail.com>
2023-07-15 11:55:32 -0700

    Added functionality to track orders in submission and idle status

--- rwa4/rwa4/orders_interface.py
+++ rwa4/rwa4/orders_interface.py
@@ -1,7 +1,8 @@
 from collections import deque
 from rclpy.node import Node
-from std_msgs.msg import UInt8
+from std_msgs.msg import UInt8, Bool
 from ariac_msgs.msg import Order as OrderMsg
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
 
 class Order:
@@ -29,17 +30,28 @@ class OrderManager(Node):
 
     def __init__(self, node_name):
         super().__init__(node_name)
+        idle_group = MutuallyExclusiveCallbackGroup()
+        submission_group = MutuallyExclusiveCallbackGroup()
         # Queue to hold regular orders
         self._order_queue = deque()
         # Queue to hold priority orders
         self._priority_queue = deque()
         self._curent_order = None
+        self._agvs_in_progress = []
         # Subscriber to listen for orders
         self._order_subscriber = self.create_subscription(
-            OrderMsg, 'order', self.order_callback, 10)
+            OrderMsg, 'ariac/orders', self.order_callback, 10)
         # Publisher to notify that order is ready to ship
         self._order_ship_publisher = self.create_publisher(
             UInt8, 'ship_order', 10)
+        # Subscriber to listen for notice of an agv submitting an order
+        self._submission_subscriber = self.create_subscription(
+            UInt8, 'agv_submit', self.order_submitted, 10, callback_group=submission_group)
+        # Publisher to notify if manager is currently idle, no orders in process
+        self._idle_publisher = self.create_publisher(
+            Bool, 'manager_idle', 10,  callback_group=idle_group)
+        self._idle_timer = self.create_timer(
+            1/10, self.idle_callback, callback_group=idle_group)
 
     def order_callback(self, order_msg: OrderMsg):
         """
@@ -56,7 +68,8 @@ class OrderManager(Node):
         else:
             # If incoming order is priority, add it to priority queue.
             # If priority queue is empty and current order is not priority,
-            # replace current order with incoming one, and move current order back to the front of the order queue.
+            # replace current order with incoming one,
+            # and move current order back to the front of the order queue.
             if order.order_priority:
                 if len(self._priority_queue == 0):
                     if not self._curent_order.order_priority:
@@ -88,28 +101,57 @@ class OrderManager(Node):
         if len(self._order_queue) != 0:
             self._order_queue.popleft()
         # If there are no orders, set current order to None
-        else:  
+        else:
             self._curent_order = None
 
-    def queues_are_empty(self) -> bool:
+    def idle_callback(self):
         """
         Function to check queue status
 
         Returns:
             bool: Returns true if both queues are empty, false otherwise
         """
-        if len(self._priority_queue) == 0 and len(self._order_queue) == 0:
-            return True
-        return False
+        # Boolean containing emptiness of both queues, true is both are empty, false otherwise
+        queues_empty = len(self._priority_queue) == 0 and len(
+            self._order_queue) == 0
+        # Boolean containing emptiness of list with agvs in progress of submission
+        # True is list is empty, false wtherwise
+        no_orders_submitting = len(self._agvs_in_progress) == 0
+        # Idle status is a Bool message containing true if both previous variables are true
+        idle_status = Bool(data=queues_empty and no_orders_submitting)
+        # Publish Bool message
+        self._idle_publisher.publish(idle_status)
 
     def complete_order(self):
         """_
         Function to notify next stage that an agv is ready to be shipped.
         """
         # Create message containing agv number of current order and publish it
-        ship_msg = UInt8(data= self._curent_order.agv_number)
+        ship_msg = UInt8(data=self._curent_order.agv_number)
+        self._agvs_in_progress.append(self._curent_order.agv_number)
         self.get_logger().info(
-            f'{self._curent_order}')
+            f'Sending {self._curent_order.agv_number} to submit order.')
         self._order_ship_publisher.publish(ship_msg)
+        self.get_logger().info(
+            f'Number of orders in progress of submission: {len(self._agvs_in_progress)}')
         # Move to start the next order
         self.next_order()
+
+    def order_submitted(self, msg: UInt8):
+        """
+        Function to update list of agvs in progress of order shipping and submission
+        Args:
+            msg (UInt8): Message containg the agv number of the order that was just submitted
+        """
+        # Grab the agv number from the message
+        order_agv_num = msg.data
+        # Remove it from the list
+        try:
+            self._agvs_in_progress.remove(order_agv_num)
+            self.get_logger().info(
+            f'Agv {order_agv_num} sucessfully submitted its order.')
+            self.get_logger().info(
+            f'Number of orders in progress of submission: {len(self._agvs_in_progress)}')
+        except ValueError:
+            self.get_logger().warn(
+                f'Agv {order_agv_num} was reported as submitted, but was not in list of orders in progress')
