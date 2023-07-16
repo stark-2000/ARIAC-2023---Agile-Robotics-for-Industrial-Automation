09f23b66ab3feb44d70c26a032772ee9163c9a1c
stark-2000 <dhineshrajasekaran@gmail.com>
2023-07-15 21:07:17 -0400

    Added gazebo launch file to make one single launch file

--- rwa4/launch/rwa4_app.launch.py
+++ rwa4/launch/rwa4_app.launch.py
@@ -1,10 +1,13 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import launch.actions
+
 
 
 def generate_launch_description():
     ld = LaunchDescription()
-
+    pkg_dir = get_package_share_directory('ariac_gazebo')
 
     service_client_exe_node = Node(
         package="rwa4",
@@ -21,11 +24,6 @@ def generate_launch_description():
         executable="ship_order_exe",
     )
 
-    service_server_submit_orders_node = Node(
-        package="rwa4",
-        executable="service_server_submit_orders.py",
-    )
-
     submit_order_node = Node(
         package="rwa4",
         executable="submit_order.py",
@@ -37,11 +35,16 @@ def generate_launch_description():
     )
     
 
+    #Launch the launch file from av_server package
+    included_launch = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+                pkg_dir + '/launch/ariac.launch.py'), launch_arguments={'trial_name': 'rwa4_summer2023'}.items())
 
+
+    ld.add_action(included_launch)
     ld.add_action(service_client_exe_node)
     ld.add_action(order_manager_node)
     ld.add_action(ship_order_exe_node)
-    ld.add_action(service_server_submit_orders_node)
     ld.add_action(submit_order_node)
     ld.add_action(end_competition_node)
     
